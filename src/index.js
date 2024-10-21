8b6197e1efab40ef19eda7b41074e5c30329efe3
netherhouse <nether3ord@gmail.com>
2024-10-21 02:28:36 +0200

    148

--- src/index.js
+++ src/index.js
@@ -1,18 +1,12 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./components/app/App";
-import MarvelService from "./services/MarvelService";
+
 import "./style/style.scss";
 
-const marvelService = new MarvelService();
 const container = document.getElementById("root");
 const root = createRoot(container);
 
-marvelService.getAllCharacters().then((res) =>
-  res.data.results.forEach((item) => {
-    console.log(item.name);
-  })
-);
 root.render(
   <React.StrictMode>
     <App />
