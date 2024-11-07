0bed0b14df454c4744a5a9515f6599850a5e43e2
netherhouse <nether3ord@gmail.com>
2024-11-07 18:13:11 +0100

    172

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -13,10 +13,21 @@ const useMarvelService = () => {
     );
     return res.data.results.map(_transformCharacter);
   };
+  const getAllComics = async (offset = 0) => {
+    const res = await request(
+      `${_apiBase}comics?orderBy=issueNumber&limit=8&offset=${offset}&${_apiKey}`
+    );
+    return res.data.results.map(_transformComics);
+  };
 
   const getCharacter = async (id) => {
     const res = await request(`${_apiBase}characters/${id}?${_apiKey}`);
     return _transformCharacter(res.data.results[0]);
+  };
+
+  const getComics = async (id) => {
+    const res = await request(`${_apiBase}comics/${id}?${_apiKey}`);
+    return _transformComics(res.data.results[0]);
   };
 
   const _transformCharacter = (char) => {
@@ -32,8 +43,32 @@ const useMarvelService = () => {
       comics: char.comics.items,
     };
   };
+  const _transformComics = (comics) => {
+    return {
+      id: comics.id,
+      title: comics.title,
+      description: comics.description || "There is no description",
+      pageCount: comics.pageCount
+        ? `${comics.pageCount} p.`
+        : "No information about the number of pages",
+      thumbnail: comics.thumbnail.path + "." + comics.thumbnail.extension,
+      language: comics.textObjects[0]?.language || "en-us",
+      // optional chaining operator
+      price: comics.prices[0].price
+        ? `${comics.prices[0].price}$`
+        : "not available",
+    };
+  };
 
-  return { loading, error, getAllCharacters, getCharacter, clearError };
+  return {
+    loading,
+    error,
+    getAllCharacters,
+    getCharacter,
+    getComics,
+    getAllComics,
+    clearError,
+  };
 };
 
 export default useMarvelService;
