b2ca06c0ea8d78e84981928c5af8861de98d67cc
netherhouse <nether3ord@gmail.com>
2025-03-29 03:10:50 +0100

    add detailed documentation for MarvelService functions

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -1,13 +1,47 @@
+/**
+ * MarvelService.js - Общее описание:
+ *
+ * Этот модуль предоставляет набор функций для взаимодействия с Marvel API,
+ * используя кастомный хук useHttp для выполнения HTTP-запросов.
+ *
+ * Основные возможности:
+ * 1. Получение списка персонажей:
+ *    - getAllCharacters: возвращает список персонажей с поддержкой пагинации.
+ *    - getCharacterByName: осуществляет поиск персонажей по имени.
+ *    - getCharacter: получает детальную информацию о конкретном персонаже по его ID.
+ *
+ * 2. Получение данных о комиксах:
+ *    - getAllComics: возвращает список комиксов, упорядоченных по номеру выпуска.
+ *    - getComic: получает детальную информацию о конкретном комиксе по его ID.
+ *
+ * 3. Преобразование данных:
+ *    - _transformCharacter: преобразует данные персонажа в удобный формат для интерфейса.
+ *    - _transformComics: преобразует данные комикса, обеспечивая удобочитаемость информации.
+ *
+ * Важные моменты:
+ * - Используется базовый URL и API-ключ для доступа к Marvel API.
+ * - Обработка ошибок и состояния запроса осуществляется через методы, возвращаемые хуком useHttp.
+ * - API-ключ непосредственно прописан в коде, что не является лучшей практикой для продакшена.
+ */
+
 import { useHttp } from "../hooks/http.hook";
 
+// Инициализируем Marvel сервис, который использует кастомный HTTP-хук
 const useMarvelService = () => {
   const { request, clearError, process, setProcess } = useHttp();
 
+  // Базовый URL для доступа к Marvel API
   const _apiBase = "https://gateway.marvel.com:443/v1/public/";
 
+  // API-ключ для аутентификации запросов к Marvel API
   const _apiKey = "apikey=a38c01a10f2e6d5dfccc506500de6d1e";
+  // Начальное смещение для пагинации списка персонажей
   const _baseOffset = 210;
 
+  /**
+   * Получает список персонажей с поддержкой пагинации.
+   * @param {number} offset - смещение для пагинации, по умолчанию _baseOffset.
+   */
   const getAllCharacters = async (offset = _baseOffset) => {
     const res = await request(
       `${_apiBase}characters?limit=9&offset=${offset}&${_apiKey}`
@@ -16,11 +50,19 @@ const useMarvelService = () => {
     return res.data.results.map(_transformCharacter);
   };
 
+  /**
+   * Ищет персонажей по имени.
+   * @param {string} name - имя персонажа для поиска.
+   */
   const getCharacterByName = async (name) => {
     const res = await request(`${_apiBase}characters?name=${name}&${_apiKey}`);
     return res.data.results.map(_transformCharacter);
   };
 
+  /**
+   * Получает детальную информацию о персонаже по его ID.
+   * @param {number} id - уникальный идентификатор персонажа.
+   */
   const getCharacter = async (id) => {
     const res = await request(`${_apiBase}characters/${id}?${_apiKey}`);
     console.log(res);
@@ -28,6 +70,10 @@ const useMarvelService = () => {
     return _transformCharacter(res.data.results[0]);
   };
 
+  /**
+   * Получает список комиксов с сортировкой по номеру выпуска.
+   * @param {number} offset - смещение для пагинации, по умолчанию 0.
+   */
   const getAllComics = async (offset = 0) => {
     const res = await request(
       `${_apiBase}comics?orderBy=issueNumber&limit=8&offset=${offset}&${_apiKey}`
@@ -35,11 +81,19 @@ const useMarvelService = () => {
     return res.data.results.map(_transformComics);
   };
 
+  /**
+   * Получает детальную информацию о комиксе по его ID.
+   * @param {number} id - уникальный идентификатор комикса.
+   */
   const getComic = async (id) => {
     const res = await request(`${_apiBase}comics/${id}?${_apiKey}`);
     return _transformComics(res.data.results[0]);
   };
 
+  /**
+   * Преобразует данные персонажа из API в удобный формат для использования в приложении.
+   * @param {object} char - данные персонажа из Marvel API.
+   */
   const _transformCharacter = (char) => {
     return {
       id: char.id,
@@ -54,6 +108,10 @@ const useMarvelService = () => {
     };
   };
 
+  /**
+   * Преобразует данные комикса из API в удобный для отображения формат.
+   * @param {object} comics - данные комикса из Marvel API.
+   */
   const _transformComics = (comics) => {
     return {
       id: comics.id,
@@ -70,6 +128,7 @@ const useMarvelService = () => {
     };
   };
 
+  // Возвращаем объект с доступными методами для использования в других частях приложения
   return {
     process,
     clearError,
