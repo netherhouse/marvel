d3d624344f3786a1cc153d56b4df431ad235b6ff
netherhouse <nether3ord@gmail.com>
2025-05-07 21:09:57 +0200

    feat: refactor App component and update entry point to TypeScript

---
+++ src/components/app/App.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+import { lazy, Suspense } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { AnimatePresence } from "framer-motion";
+
+import AppHeader from "../appHeader/AppHeader";
+import Spinner from "../spinner/Spinner";
+
+const Page404 = lazy(() => import("../pages/404"));
+const MainPage = lazy(() => import("../pages/MainPage"));
+const ComicsPage = lazy(() => import("../pages/ComicsPage"));
+const SingleComicPage = lazy(
+  () => import("../pages/singleComicLayout/SingleComicPage")
+);
+const SingleCharachterLayout = lazy(
+  () => import("../pages/singleCharachterLayout/SingleCharachterLayout")
+);
+
+const App = () => {
+  return (
+    <Router>
+      <div className="app">
+        <AppHeader />
+        <main>
+          <Suspense fallback={<Spinner />}>
+            <AnimatePresence>
+              <Routes>
+                <Route path="/" element={<MainPage />} />
+                <Route path="/comics" element={<ComicsPage />} />
+                <Route path="/comics/:comicId" element={<SingleComicPage />} />
+                <Route
+                  path="/char/:charId"
+                  element={<SingleCharachterLayout />}
+                />
+                <Route path="*" element={<Page404 />} />
+              </Routes>
+            </AnimatePresence>
+          </Suspense>
+        </main>
+      </div>
+    </Router>
+  );
+};
+
+export default App;
