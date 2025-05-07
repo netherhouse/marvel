0e5cbc780edd4c5bb9af182ac1e82f3f59da6d4c
netherhouse <nether3ord@gmail.com>
2025-05-07 21:15:11 +0200

    feat: add RandomChar component with character fetching logic and styling, implement setContent utility for loading states

--- src/components/randomChar/RandomChar.tsx
+++ src/components/randomChar/RandomChar.tsx
@@ -1,34 +1,31 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 
-import useMarvelService from "../../services/MarvelService";
+import useMarvelService from "../../services/marvelService";
 import setContent from "../../utils/setContent";
 
 import "./randomChar.scss";
-
 import mjolnir from "../../resources/img/mjolnir.png";
 
-const RandomChar = () => {
+const RandomChar: React.FC = () => {
   const [char, setChar] = useState({});
-
   const { getCharacter, clearError, process, setProcess } = useMarvelService();
 
   useEffect(() => {
     updateChar();
-    // const timerId = setInterval(updateChar, 60000);
+    const timerId = setInterval(updateChar, 50000);
 
-    // return () => {
-    //   clearInterval(timerId);
-    // };
+    return () => {
+      clearInterval(timerId);
+    };
   }, []);
 
-  const onCharLoaded = (char) => {
+  const onCharLoaded = (char: Char) => {
     setChar(char);
   };
 
   const updateChar = () => {
     clearError();
     const id = Math.floor(Math.random() * (1011400 - 1011000) + 1011000);
-
     getCharacter(id)
       .then(onCharLoaded)
       .then(() => setProcess("confirmed"));
@@ -53,32 +50,34 @@ const RandomChar = () => {
   );
 };
 
-const View = ({ data }) => {
+interface ViewProps {
+  data: Char;
+}
+
+const View: React.FC<ViewProps> = ({ data }) => {
   const { name, description, thumbnail, homepage, wiki } = data;
-  let imgStyle = { objectFit: "cover" };
+  let classNames = "";
+
   if (
     thumbnail ===
     "http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg"
   ) {
-    imgStyle = { objectFit: "contain" };
+    classNames = "randomchar__img object-fit-contain";
+  } else {
+    classNames = "randomchar__img object-fit-cover";
   }
 
   return (
     <div className="randomchar__block">
-      <img
-        src={thumbnail}
-        alt="Random character"
-        className="randomchar__img"
-        style={imgStyle}
-      />
+      <img src={thumbnail} alt="Random character" className={classNames} />
       <div className="randomchar__info">
         <p className="randomchar__name">{name}</p>
         <p className="randomchar__descr">{description}</p>
         <div className="randomchar__btns">
-          <a href={homepage} className="button button__main">
+          <a target="_blank" href={homepage} className="button button__main">
             <div className="inner">homepage</div>
           </a>
-          <a href={wiki} className="button button__secondary">
+          <a target="_blank" href={wiki} className="button button__secondary">
             <div className="inner">Wiki</div>
           </a>
         </div>
